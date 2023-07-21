3cbcbbfc7aceaf0791dfe6d60e4a8a6cb8d83d91
Salma Ahmed <107444767+Sallmahussien@users.noreply.github.com>
2023-07-22 02:41:26 +0300

    Fix insertion sort bug

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,43 +1,6 @@
 #include "sort.h"
 
 /**
- * swap_nodes - exchange two nodes in linked list
- * @list: list to be sorted
- * @curr_node: current node
- * @prev_node: previous node
- */
-void swap_nodes(listint_t **list, listint_t  *curr_node, listint_t *prev_node)
-{
-	if (prev_node->prev)
-		prev_node->prev->next = curr_node;
-	else
-		*list = curr_node;
-
-	if (curr_node->next)
-		curr_node->next->prev = prev_node;
-
-	prev_node->next = curr_node->next;
-	curr_node->prev = prev_node->prev;
-	prev_node->prev = curr_node;
-	curr_node->next = prev_node;
-}
-
-/**
- * is_small_list - checks if list have less than two element or not
- * @list: list to be sorted
- * Return: true if it has less than two elements, otherwise false
- */
-bool is_small_list(listint_t **list)
-{
-	if (!(*list)->next)
-		return (true);
-	if (!(*list)->next->next)
-		return (true);
-
-	return (false);
-}
-
-/**
  * insertion_sort_list - algorithm
  * @list: list to be sorted
  */
@@ -63,5 +26,39 @@ void insertion_sort_list(listint_t **list)
 		}
 		current_node = current_node->next;
 	}
+}
 
+/**
+ * is_small_list - checks if list have less than two element or not
+ * @list: list to be sorted
+ * Return: true if it has less than two elements, otherwise false
+ */
+bool is_small_list(listint_t **list)
+{
+	if (!(*list)->next)
+		return (true);
+
+	return (false);
+}
+
+/**
+ * swap_nodes - exchange two nodes in linked list
+ * @list: list to be sorted
+ * @curr_node: current node
+ * @prev_node: previous node
+ */
+void swap_nodes(listint_t **list, listint_t  *curr_node, listint_t *prev_node)
+{
+	if (prev_node->prev)
+		prev_node->prev->next = curr_node;
+	else
+		*list = curr_node;
+
+	if (curr_node->next)
+		curr_node->next->prev = prev_node;
+
+	prev_node->next = curr_node->next;
+	curr_node->prev = prev_node->prev;
+	prev_node->prev = curr_node;
+	curr_node->next = prev_node;
 }
